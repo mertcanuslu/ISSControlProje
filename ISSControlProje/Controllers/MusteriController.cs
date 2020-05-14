48f62d562430d7a52e8047f1e0f76b4152aa15f9
Mert Can Uslu <mertcanuslu18@gmail.com>
2020-05-14 12:33:26 +0300

    Sil güncelle ve yeni ekle butonlarıyla birlikte view'lar tamamlandı.

---
+++ ISSControlProje/Controllers/MusteriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISSControlProje.Models.Entity;
+
+namespace ISSControlProje.Controllers
+{
+    public class MusteriController : Controller
+    {
+        iss_webEntities vt = new iss_webEntities();
+
+        // GET: Musteri
+        public ActionResult Index()
+        {
+            return View(vt.tblMusteriler.ToList());
+        }
+    }
+}
